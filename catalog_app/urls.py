c55f7c73b8196fcd41e773391a478b9bca6985e6
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-07-03 10:53:53 +0300

    add category route

--- catalog_app/urls.py
+++ catalog_app/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from catalog_app.views import ManufacturerView, GoodView, UpdateCatalogView, DataView
+from catalog_app.views import (
+    ManufacturerView,
+    GoodView,
+    UpdateCatalogView,
+    DataView,
+    CategoryView,
+)
 
 
 app_name = "catalog_app"
 
 urlpatterns = [
     path("manufacturer/", ManufacturerView.as_view(), name="manufacturer"),
+    path("category/", CategoryView.as_view(), name="category"),
     path("good/", GoodView.as_view(), name="good"),
     path("data/", DataView.as_view(), name="data"),
     path("update-catalog/", UpdateCatalogView.as_view(), name="update-catalog"),
