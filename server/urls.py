608fe5a33918bca3cf02b044e77ef6fd7f9749da
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-04 10:58:16 +0300

    refactoring

--- server/urls.py
+++ server/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('auth/', include('auth_app.urls', namespace='auth_app')),
-    path('api/v1/', include('api_app.urls', namespace='api_app')),
-    path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
- + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("api/v1/", include("api_app.urls", namespace="api_app")),
+        path("admin/", admin.site.urls),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
