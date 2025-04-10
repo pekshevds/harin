3806f5f2e98015ac436c983c2886e3ed913074b0
denis sergeevich pekshev <pekshev.ds@gmail.com>
2025-04-10 15:29:05 +0300

    create yml_catalog.xml and download

--- server/urls.py
+++ server/urls.py
@@ -36,4 +36,4 @@ urlpatterns = (
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
-print(settings.STATIC_ROOT)
+print(settings.MEDIA_ROOT)
